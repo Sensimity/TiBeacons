78b513985126d1cc0613ae4bdaa07cd5928fadcd
William Rijksen <williamrijksen@gmail.com>
2016-09-08 15:16:27 +0200

    cleanup unnecessary code and prepare for iOS10

--- Classes/OrgBeuckmanTibeaconsModule.h
+++ Classes/OrgBeuckmanTibeaconsModule.h
@@ -1,9 +1,17 @@
-
 #import "TiModule.h"
-#import "LocationManagerDelegateProxy.h"
 
 @import CoreLocation;
 @import CoreBluetooth;
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED <= __IPHONE_9_3
+#define CBManagerState CBCentralManagerState
+#define CBManagerStateUnknown CBCentralManagerStateUnknown
+#define CBManagerStateResetting CBCentralManagerStateResetting
+#define CBManagerStateUnsupported CBCentralManagerStateUnsupported
+#define CBManagerStateUnauthorized CBCentralManagerStateUnauthorized
+#define CBManagerStatePoweredOff CBCentralManagerStatePoweredOff
+#define CBManagerStatePoweredOn CBCentralManagerStatePoweredOn
+#endif
 
 @interface OrgBeuckmanTibeaconsModule : TiModule <CLLocationManagerDelegate, CBPeripheralManagerDelegate, CBCentralManagerDelegate>
 {
